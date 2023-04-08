9b113d09c3012162d8a9be7eac20b722f2b47c4a
juheyi <reznik.nikkita@gmail.com>
2023-04-08 11:31:52 +0300

    tests

---
+++ tests.py
@@ -0,0 +1,18 @@
+import unittest
+class My_Test(unittest.TestCase):
+    class My_Test(unittest.TestCase):
+        def fibonacci():
+            prew = cur = 1
+            elements = [0, 1, 1]
+            for n in range(100):
+                tmp = prew + cur
+                prew = cur
+                cur = tmp
+                elements.append(cur)
+                print(elements)
+    def speed_test():
+        t0 = time.time()
+        fibonacci()
+        t1 = time.time()
+        total = t1-t0
+        print(total)
